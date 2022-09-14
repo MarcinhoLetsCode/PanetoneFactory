4b580078555d842e2506e584442203b2244b1b4e
MarcinhoLetsCode <darlan_lifecat@yahoo.com.br>
2022-09-13 22:22:54 -0300

    Adapter Implementation

--- app/src/main/java/br/com/etecia/panetonefactory/coffe_fragment.java
+++ app/src/main/java/br/com/etecia/panetonefactory/coffe_fragment.java
@@ -2,15 +2,14 @@ package br.com.etecia.panetonefactory;
 
 import android.content.Context;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,39 +23,54 @@ public class coffe_fragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_coffe_bottom, container, false);
 
         recyclerView = view.findViewById(R.id.rvCoffe);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        recyclerView.hasFixedSize();
 
         cafeLista = new ArrayList<>();
         cafeLista.add(new Coffe("Café Preto"));
+        cafeLista.add(new Coffe("Café Branco"));
+        cafeLista.add(new Coffe("Café Verde"));
+        cafeLista.add(new Coffe("Café Vermelho"));
 
-        CoffeAdapter coffeAdapter = new CoffeAdapter( getContext(), cafeLista);
+        CoffeAdapter coffeAdapter = new CoffeAdapter(getContext(), cafeLista);
         recyclerView.setAdapter(coffeAdapter);
 
         return view;
     }
 
     private class CoffeAdapter extends RecyclerView.Adapter<CoffeAdapter.ViewHolder> {
+        List<Coffe> cafeLista;
+        Context context;
+
         public CoffeAdapter(Context context, List<Coffe> cafeLista) {
+            this.context = context;
+            this.cafeLista = cafeLista;
         }
 
         @NonNull
         @Override
         public CoffeAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+            View view;
+            LayoutInflater inflater = LayoutInflater.from(context);
+            view = inflater.inflate(R.layout.fragment_coffe_bottom, parent, false);
+            return new ViewHolder(view);
         }
 
         @Override
         public void onBindViewHolder(@NonNull CoffeAdapter.ViewHolder holder, int position) {
-
+            holder.textView.setText(cafeLista.get(holder.getAdapterPosition()).getCafe());
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return cafeLista.size();
         }
 
         public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView textView;
             public ViewHolder(@NonNull View itemView) {
                 super(itemView);
+                textView = itemView.findViewById(R.id.txtNome);
             }
         }
     }
